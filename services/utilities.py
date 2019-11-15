cafb7a14b29fe361eee7f5ad037f42184181c77e
snehasri25 <ssrivast@tcd.ie>
2019-11-15 04:38:10 +0000

    recommender system

--- services/utilities.py
+++ services/utilities.py
@@ -28,31 +28,33 @@ def calculate_similarity(data_df,ideal_df):
     l1 = len(data_df)
     for i in range(l1):
         arr1 = ideal_df.iloc[i, :].values
+        user_id_1 = ideal_df.index[i]
         score_other_list = []
         for j in range(i + 1, l1):
             arr2 = data_df.iloc[j, :].values
             score_other_list.append(euclidean_distance(arr1, arr2))
-        score_dict[i] = score_other_list
-    del score_dict[i]
+        score_dict[user_id_1] = score_other_list
+    #del score_dict[user_id_1]
     return score_dict
 
-def find_match_score(self_score,ideal_score):
-    # finding match score
+
+def find_match_score(self_score, ideal_score):
     match_score = {}
-    # fetch individual scores and minimize them
-    temp, temp1 = 0, 0
-    l1 = len(self_score)
-    for i in range(l1):
-        temp = self_score[i]
-        temp1 = ideal_score[i]
+    temp1, temp2 = 0, 0
+    f = 1
+    for key1, values1 in self_score.items():
+        if (len(values1) == 0):
+            continue
+        temp1 = self_score[key1]
+        temp2 = ideal_score[key1]
         temp_list = []
-        for j in range(len(temp)):
-            temp_list.append(((temp[j] + temp1[j]) // 2))
+        for value1, value2 in zip(temp1, temp2):
+            temp_list.append((value1 + value2) // 2)
             m = max(temp_list)
-            # storing the maximum match score and indices for it
-        match_score[i] = [m, [x for x, y in enumerate(temp_list) if y == m]]
-        # print(temp_list)
+        match_score[key1] = [m, [list(self_score.keys())[x + f] for x, y in enumerate(temp_list) if y == m]]
+        f = f + 1
     return match_score
+
 
 def main():
     arr1 = []
