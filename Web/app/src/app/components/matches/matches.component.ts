3fb90210dbec1d04b7fbf8e2d8aea9320a2c00fc
aashimagoel <aashima.goel91@gmail.com>
2019-11-26 20:45:10 +0000

    changes

--- Web/app/src/app/components/matches/matches.component.ts
+++ Web/app/src/app/components/matches/matches.component.ts
@@ -14,36 +14,6 @@ export class MatchesComponent implements OnInit {
 
   ngOnInit() {
     this.fetchMatchData();
-    this.matchData = [
-      {
-        'match_name' : "Ellen",
-        'match_ph' : "2432534242",
-        'activity_cat' : "Beach",
-        'activity_name' : 'Blash Blah',
-        'activity_add' : '123 ABC St, Dublin 2'
-      },
-      {
-        'match_name' : "Mark",
-        'match_ph' : "2432534242",
-        'activity_cat' : "Beach",
-        'activity_name' : 'Blash Blah',
-        'activity_add' : '123 ABC St, Dublin 2'
-      },
-      {
-        'match_name' : "Ella",
-        'match_ph' : "3253454232",
-        'activity_cat' : "Beach",
-        'activity_name' : 'Blash wqrwsdw Blah',
-        'activity_add' : '123 ABC St'
-      },
-      {
-        'match_name' : "Mark Spencer",
-        'match_ph' : "42354534423",
-        'activity_cat' : "Beach",
-        'activity_name' : 'Blash Blah',
-        'activity_add' : 'Dublin 2'
-      }
-    ];
   }
 
   fetchMatchData(){
